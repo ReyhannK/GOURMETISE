d424c2950cbe4d8676c3548b2419d1d93eda45d6
Reyhan <KARABENLI@btssio.local>
2024-11-22 12:56:49 +0100

    fin des api post de user et bakery + debut du formulaire

--- GourmetiseVue/src/router/index.js
+++ GourmetiseVue/src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Index from "@/views/index.vue";
 import ContestParams from "@/views/contestParams.vue";
+import SignIn from "@/views/formSign-in.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: '/contestParams',
             name: 'contestParams',
             component: ContestParams,
+        },
+        {
+            path: '/sign-in',
+            name: 'Sign-in',
+            component: SignIn,
         },
     ],
 })
---
+++ GourmetiseVue/src/views/formSign-in.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container>
+    <h1>S'inscrire au concours</h1>
+    <form @submit.prevent="submit">
+      <v-text-field
+        v-model="siret"
+        :error-messages="nameError ? 'Name needs to be at least 2 characters.' : ''"
+        label="N°Siret"
+        type="input"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="name"
+        :error-messages="nameError ? 'Name needs to be at least 2 characters.' : ''"
+        label="Name"
+        type="input"
+        dense 
+      ></v-text-field>
+
+      <v-text-field
+        v-model="street"
+        :error-messages="nameError ? 'Name needs to be at least 2 characters.' : ''"
+        label="Rue"
+        type="input"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="postal_code"
+        :error-messages="nameError ? 'Name needs to be at least 2 characters.' : ''"
+        label="Code postal"
+        type="input"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="city"
+        :error-messages="nameError ? 'Name needs to be at least 2 characters.' : ''"
+        label="Ville"
+        type="input"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="telephone_number"
+        :error-messages="phoneError ? 'Phone number needs to be at least 7 digits.' : ''"
+        label="Numéro téléphone"
+        type="input"
+      ></v-text-field>
+      
+      <v-text-field
+        v-model="contact_name"
+        :error-messages="phoneError ? 'Phone number needs to be at least 7 digits.' : ''"
+        label="Nom contact"
+        type="input"
+      ></v-text-field>
+
+      <v-textarea
+        v-model="bakery_description"
+        :error-messages="phoneError ? 'Phone number needs to be at least 7 digits.' : ''"
+        label="Description boulangerie"
+      ></v-textarea>
+
+      <v-textarea
+        v-model="products_decription"
+        :error-messages="phoneError ? 'Phone number needs to be at least 7 digits.' : ''"
+        label="Description des produits"
+      ></v-textarea>
+
+      <v-checkbox
+        v-model="checkbox"
+        :error-messages="checkboxError ? 'Must be checked.' : ''"
+        label="Option"
+        type="checkbox"
+        value="1"
+      ></v-checkbox>
+
+      <v-btn class="me-4" type="submit">
+        Submit
+      </v-btn>
+    </form>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const siret = ref('')
+const name = ref('')
+const street = ref('')
+const postal_code = ref('')
+const city = ref('')
+const telephone_number = ref('')
+const contact_name = ref('')
+const bakery_description = ref('')
+const products_decription = ref('')
+const checkbox = ref(false)
+
+</script>
+
+<style scoped>
+.my-12 {
+  margin-bottom: 1rem;
+}
+
+.mt-10 {
+  margin-top: 2rem;
+}
+</style>
